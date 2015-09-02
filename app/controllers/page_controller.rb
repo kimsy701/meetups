3758ea15a9a34b244a8c1423ba6f88eeb17731b8
Nicholas Kim <nicholaskim94@gmail.com>
2015-09-02 18:51:31 +0900

    Add bootstrap support to frontend
    Add models user and meetup to backend

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,13 @@
+class PageController < ApplicationController
+	def index
+	end
+
+	def profile
+	end
+
+	def category
+	end
+
+	def meetup
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root :to => "page#index"
+  match ":controller(/:action(/:id))", :via => [:post, :get]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
